8a82c0d7e72aa85fc70cd36aff3de8fb3cb019bb
Kyle Rego <regoky@outlook.com>
2023-07-08 12:11:33 -0400

    Add test script

--- bin/test_scripts/test_0.rb
+++ bin/test_scripts/test_0.rb
@@ -6,7 +6,7 @@ FileUtils.rm_f("test_0.apkg")
 
 apkg = AnkiRecord::AnkiPackage.create(name: "test_0")
 anki21_database = apkg.anki21_database
-custom_nested_deck = AnkiRecord::Deck.new(name: "parent::child::nested_deck::super nested", anki21_database:)
+custom_nested_deck = AnkiRecord::Deck.new(name: "test_0_parent::child::nested_deck::super nested", anki21_database:)
 custom_nested_deck.save
 basic_note_type = anki21_database.find_note_type_by name: "Basic"
 note = AnkiRecord::Note.new(deck: custom_nested_deck, note_type: basic_note_type)
